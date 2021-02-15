dc692807b44ccd4f33d42e6b4d8565505b9a5b28
Michael Fuget <michaelfuget@gmail.com>
2021-02-15 02:25:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ class Polygon{
 class Triangle extends Polygon
 {
   get isValid(){
-    console.log(this.sides[2]+this.sides[1]>this.sides[0])
+    return (this.countSides == 3 && this.sides[0]+this.sides[1]>this.sides[2] && this.sides[0]+this.sides[2]>this.sides[1] && this.sides[2]+this.sides[1]>this.sides[0])
+  }
+}
+class Square extends Polygon
+{
+  get isValid(){
     return (this.countSides == 3 && this.sides[0]+this.sides[1]>this.sides[2] && this.sides[0]+this.sides[2]>this.sides[1] && this.sides[2]+this.sides[1]>this.sides[0])
   }
 }
